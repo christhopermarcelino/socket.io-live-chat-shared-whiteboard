b621b7bb851f3507fab2ee2b4cbbaf48367476cb
christhopermarcelino <christhopermarcelino@gmail.com>
2022-03-04 18:52:13 +0700

    [B] chore: install and set up socket.io

--- server/index.js
+++ server/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} connected to server`);
+
+  io.on('disconnect', (reason) =>
+    console.log(`${reason}: User ${socket.id} disconnected from server`)
+  );
+});
+
+httpServer.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
