339822a634400bf8010157762a7c7b905678ef94
christhopermarcelino <christhopermarcelino@gmail.com>
2022-03-10 21:51:00 +0700

    [F] feat: remove nextjs project files

--- client/script.js
+++
@@ -1,7 +0,0 @@
-// import { io } from 'socket.io-client';
-// console.log(io);
-const socket = io('http://localhost:3001', { autoConnect: false });
-
-const res = confirm('Connect?');
-
-if (res) socket.connect();
