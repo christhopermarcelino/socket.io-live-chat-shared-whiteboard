920c5ca112eed52efaa57eceb5d304ec14d660cc
christhopermarcelino <christhopermarcelino@gmail.com>
2022-03-10 23:43:15 +0700

    [F] feat: connect service to join room

--- client/script.js
+++ client/script.js
@@ -1,28 +1,61 @@
 const socket = io('http://localhost:3001');
 
 const createRoomForm = document.getElementById('create-room-form');
-const creatorName = document.getElementById('username');
-const creatorRoom = document.getElementById('room');
+const username = document.getElementById('username');
+const room = document.getElementById('room');
 const isPublic = document.getElementById('isPublic-checkbox');
 const isOpen = document.getElementById('isOpen-checkbox');
 
-createRoomForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+const joinRoomForm = document.getElementById('join-room-form');
 
-  if (creatorName.value === '' || creatorRoom.value === '') return;
+createRoomForm &&
+  createRoomForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  const data = {
-    id: socket.id,
-    username: creatorRoom.value,
-    room: creatorRoom.value,
-    isPublic: !isPublic.checked,
-    isOpen: !isOpen.checked,
-  };
+    if (username.value === '' || room.value === '') return;
 
-  socket.emit('create-room', data);
-});
+    const data = {
+      id: socket.id,
+      username: username.value,
+      room: room.value,
+      isPublic: !isPublic.checked,
+      isOpen: !isOpen.checked,
+    };
+
+    socket.emit('create-room', data);
+  });
 
 socket.on('create-room', (data) => {
+  console.log(data);
+  const pResponse = document.createElement('p');
+
+  pResponse.innerText = data.message;
+  pResponse.classList.add('response-text');
+  data.success
+    ? pResponse.classList.add('bg-success')
+    : pResponse.classList.add('bg-error');
+  document.body.prepend(pResponse);
+
+  if (data.success) {
+    window.location.href = 'http://127.0.0.1:5500/client/chat.html';
+  }
+});
+
+joinRoomForm &&
+  joinRoomForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (username.value === '' || room.value === '') return;
+
+    const data = {
+      username: username.value,
+      room: room.value,
+    };
+
+    socket.emit('join-room', data);
+  });
+
+socket.on('join-room', (data) => {
   console.log(data);
   const pResponse = document.createElement('p');
 
