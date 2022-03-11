b1522ba9c59627bc0c94ad77bf9d68e38a11b8be
christhopermarcelino <christhopermarcelino@gmail.com>
2022-03-11 20:15:55 +0700

    [F] feat: add custom font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
